7c585c6a35c036d702e1b3142884c5ea666f7c0a
Gregor <gregor.mitscha-baude@gmx.at>
2021-02-22 16:49:17 +0100

    change index.js

--- index.js
+++ index.js
@@ -1,118 +1,73 @@
-var events = require('events')
-var ess = require('event-source-stream')
-var nets = require('nets')
-var pump = require('pump')
-var through = require('through2')
-var inherits = require('inherits')
+// adapted from mafintosh/signalhub/index.js to work standalone in the browser
+export default (...args) => new SignalHub(...args);
 
-module.exports = SignalHub
+class SignalHub {
+  constructor(app, url) {
+    if (!app) throw new Error('app name required');
+    if (!url) throw new Error('signalhub url required');
 
-function SignalHub (app, urls) {
-  if (!(this instanceof SignalHub)) return new SignalHub(app, urls)
-  if (!app) throw new Error('app name required')
-  if (!urls || !urls.length) throw new Error('signalhub url(s) required')
-
-  events.EventEmitter.call(this)
-  this.setMaxListeners(0)
-
-  this.app = app
-  if (!Array.isArray(urls)) urls = [urls]
-  this.urls = urls.map(function (url) {
-    url = url.replace(/\/$/, '')
-    return url.indexOf('://') === -1 ? 'http://' + url : url
-  })
-  this.subscribers = []
-  this.closed = false
-}
-
-inherits(SignalHub, events.EventEmitter)
-
-SignalHub.prototype.subscribe = function (channel) {
-  if (this.closed) throw new Error('Cannot subscribe after close')
-
-  var self = this
-  var endpoint = Array.isArray(channel) ? channel.join(',') : channel
-  var streams = this.urls.map(function (url) {
-    return ess(url + '/v1/' + self.app + '/' + endpoint, {json: true})
-  })
-
-  var subscriber
-  if (streams.length === 1) {
-    subscriber = streams[0]
-  } else {
-    subscriber = through.obj()
-    subscriber.setMaxListeners(0)
-    streams.forEach(function (stream) {
-      stream.on('open', function () {
-        subscriber.emit('open')
-      })
-      pump(stream, subscriber)
-    })
+    this.app = app;
+    url = url.replace(/\/$/, '');
+    this.url = url.indexOf('://') === -1 ? 'http://' + url : url;
+    this.subscribers = [];
+    this.closed = false;
   }
 
-  this.subscribers.push(subscriber)
+  subscribe(channel, onMessage) {
+    if (this.closed) throw new Error('Cannot subscribe after close');
 
-  subscriber.once('close', function () {
-    var i = self.subscribers.indexOf(subscriber)
-    if (i > -1) self.subscribers.splice(i, 1)
-  })
+    let endpoint = Array.isArray(channel) ? channel.join(',') : channel;
+    let subscriber = new EventSource(
+      this.url + '/v1/' + this.app + '/' + endpoint
+    );
+    subscriber.addEventListener('message', e => {
+      onMessage(decode(e.data));
+    });
 
-  return subscriber
-}
+    this.subscribers.push(subscriber);
+    return subscriber;
+  }
 
-SignalHub.prototype.broadcast = function (channel, message, cb) {
-  if (this.closed) throw new Error('Cannot broadcast after close')
-  if (!message) message = {}
-  if (!cb) cb = noop
+  // TODO: this is untested as we ended up not needing it
+  unsubscribe(subscriber) {
+    subscriber.close();
+    arrayRemove(this.subscribers, subscriber);
+  }
 
-  var pending = this.urls.length
-  var errors = 0
+  async broadcast(channel, message = {}) {
+    if (this.closed) throw new Error('Cannot broadcast after close');
+    let url = this.url + '/v1/' + this.app + '/' + channel;
+    let res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        accept: 'application/json',
+      },
+      body: JSON.stringify(message),
+    });
+    if (!res.ok) {
+      throw new Error('Bad status: ' + res.status);
+    }
+  }
 
-  var self = this
-  this.urls.forEach(function (url) {
-    broadcast(self.app, url, channel, message, function (err) {
-      if (err) errors++
-      if (--pending) return
-      if (errors === self.urls.length) return cb(err)
-      cb()
-    })
-  })
-}
-
-SignalHub.prototype.close = function (cb) {
-  if (this.closed) return
-  this.closed = true
-
-  if (cb) this.once('close', cb)
-  var len = this.subscribers.length
-  if (len > 0) {
-    var self = this
-    var closed = 0
-    this.subscribers.forEach(function (subscriber) {
-      subscriber.once('close', function () {
-        if (++closed === len) {
-          self.emit('close')
-        }
-      })
-      process.nextTick(function () {
-        subscriber.destroy()
-      })
-    })
-  } else {
-    this.emit('close')
+  close() {
+    if (this.closed) return;
+    this.closed = true;
+    this.subscribers.forEach(subscriber => {
+      subscriber.close();
+    });
   }
 }
 
-function broadcast (app, url, channel, message, cb) {
-  return nets({
-    method: 'POST',
-    json: message,
-    url: url + '/v1/' + app + '/' + channel
-  }, function (err, res) {
-    if (err) return cb(err)
-    if (res.statusCode !== 200) return cb(new Error('Bad status: ' + res.statusCode))
-    cb()
-  })
+function decode(data) {
+  try {
+    return JSON.parse(data);
+  } catch (err) {
+    return undefined;
+  }
 }
 
-function noop () {}
+function arrayRemove(arr, el) {
+  let i = arr.indexOf(el);
+  if (i !== -1) arr.splice(i, 1);
+}
